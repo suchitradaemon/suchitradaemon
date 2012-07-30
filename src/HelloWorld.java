6f4dc973e46f70c7b864c360738bd964d210282a
Suchitra nair <Suchitradaemon@gmail.com>
2012-07-30 15:43:18 +0530

    Checking the commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World!!!");
 		System.out.println("Added one more line");
+		System.out.println("One more line it is ...");
 	}
 }
